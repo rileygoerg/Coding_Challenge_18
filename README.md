b5ab687e67daab8bb952c9c5e46190e768943605
rileygoerg <rgoerg@usf.edu>
2024-07-16 19:30:32 -0400

    Implement RateMovieButton with user feedback.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Coding_Challenge_18
+## U14123683
+Develop an enhanced version of the platform's toolbar by incorporating new features that boost user interaction and provide a seamless experience.
---
+++ my-app/RateMovieButton.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+const RateMovieButton = ({movieId}) => {
+  const [rating, setRating] = useState(null);
+  const [message, setMessage] = useState('');
+
+  const movieRating = (rate) => {
+    setRating(rate);
+    setMessage(`You rated this movie ${rate} stars.`);
+  };
+
+  return (
+    <div>
+      {[1, 2, 3, 4, 5].map((stars) => (
+        <button key={stars} onClick={() => movieRating(stars)}>
+          {stars} Star{stars > 1 && 's'}
+        </button>
+      ))}
+      {message && <p>{message}</p>}
+    </div>
+  );
+};
+
+export default RateMovieButton;
