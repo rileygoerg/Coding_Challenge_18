7549ab25c5c4c38347be4606ec2beaab07d8cac8
rileygoerg <rgoerg@usf.edu>
2024-07-16 19:44:53 -0400

    Enhance UploadButton with simplified user feedback.

---
+++ my-app/src/UploadButton.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+export default function UploadButton(){
+  const [uploading, setUploading] = useState(false);
+
+  function upload() {
+    setUploading(true);
+    alert('Uploading Image');
+    setUploading(false);
+  };
+
+  return (
+    <button onClick={upload} disabled={uploading}>
+      {uploading ? 'Uploading Image' : 'Upload Image'}
+    </button>
+  );
+};
